bca3bf3ff46c9ac1e7e4c14760ff99908be37685
Lee Przybylski <przybylskily@gmail.com>
2023-07-02 21:51:13 -0400

    Added mathutils

---
+++ R/math_utils.R
@@ -0,0 +1,15 @@
+#' A logistic function to get binomial probability team 1 wins
+#'
+#' @param a1 Linear value of team 1
+#' @param a2 Linear value of team 2
+#'
+#' @return The probability that team 1 wins according to a logistic model
+#' @export)
+#'
+#' @examples
+#' logistic(1, 0)
+#' 
+logistic <- function(a1 = 0, a2 = 0){
+  p <- 1/(1+ exp(a2 - a1))
+  return(p)
+}
