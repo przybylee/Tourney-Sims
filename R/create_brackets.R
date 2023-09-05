f33716ccbcbc2d4d82a0aa0f93d9e2fbbf3332cb
Lee Przybylski <przybylskily@gmail.com>
2023-09-04 22:17:25 -0400

    Added documentation

--- R/create_brackets.R
+++ R/create_brackets.R
@@ -1,6 +1,17 @@
-entries <- 16
-round <- 1
+# entries <- 16
+# round <- 1
 
+#' Created match rows for a round
+#'
+#' @param round Integer indciating the round of the tournament
+#' @param entries Integer indicating the number of entries remaining in the 
+#' tournament
+#'
+#' @return A dataframe with 4 fieldws
+#' @export
+#'
+#' @examples
+#' #none
 build_round <- function(round, entries){
   matches <- data.frame(round = round, id = 1:(entries/2)) %>% 
     as_tibble() %>% 
