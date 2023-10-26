0e880cc0863573570ba4edb99ee2123611c2523a
Lee Przybylski <przybylskily@gmail.com>
2023-10-26 10:19:07 -0400

    Added raw data file for sample matches

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,20 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `sample_matches` dataset goes here
 ?play_singles_match
+
+sample_matches <- data.frame(
+  g_max = rep(3, 10),
+  f_score = 11,
+  serve1 = rnorm(10, 0, 0.1), 
+  serve2 = rnorm(10, 0, 0.1) 
+  
+           ) %>% 
+  mutate(return1 = serve1 + rnorm(10, 0.01, 0.05),
+         return2 = serve2 + rnorm(10, 0.01, 0.05)
+  ) %>% 
+  as_tibble()
+
+sample_matches
+
 usethis::use_data(sample_matches, overwrite = TRUE)
+
+?purrr::pmap_dfr
