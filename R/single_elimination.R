6dba9313cd3149b5876c2c49effffe4d2c6ee3a8
Lee Przybylski <przybylskily@gmail.com>
2023-12-30 23:22:01 -0500

    Added play round and update standings method

--- R/single_elimination.R
+++ R/single_elimination.R
@@ -40,11 +40,11 @@ single_elim_1x <- R6::R6Class(
     #'
     #' @return A new instance of the single elimination singles R6 class
     #'
-    initialize = function(entries){
-      # #Draw entries if not specified
-      # if(is.null(entries)){
-      #   entries <- draw_singles_entries()
-      # }
+    initialize = function(entries = NULL){
+      #Draw entries if not specified
+      if(is.null(entries)){
+        entries <- draw_singles_entries()
+      }
       
       #Maybe add a distortion for the seeding here?
       
@@ -89,7 +89,7 @@ single_elim_1x <- R6::R6Class(
     #' Update the tournament standings based on match results from the last round
     #' @param prev_results Tibble with the results from the last round of matches
     #'
-    update_standings <- function(prev_results){
+    update_standings = function(prev_results){
       self$standings <- self$standings %>% 
         left_join(select(prev_results, pos_winner, match) %>% mutate(win = 1), 
                   by = c("pos" = "pos_winner")
@@ -116,7 +116,7 @@ single_elim_1x <- R6::R6Class(
     #' @param prev_results Tibble with the results from the last round of matches
     #' 
     #If the number of matches played is greater than 1 when it should be one, award medals
-    update_standings_final <- function(prev_results){
+    update_standings_final = function(prev_results){
       self$standings <- self$standings %>%
         left_join(select(prev_results, pos_winner, match) %>% mutate(win = 1), 
                   by = c("pos" = "pos_winner")
@@ -133,11 +133,56 @@ single_elim_1x <- R6::R6Class(
         select(-contains("match"), -win, -lose, -pos2)
       
       return(invisible())
+    },
+    
+    #' @description Play round and update the standings
+    #'
+    play_round_and_update_standings = function(){
+      
+      round_matches <- self$get_next_round_matches()
+      #If this is the last round, add a bronze medal match
+      if(N_matches_round == 1){
+        bronze_match <- standings %>% 
+          filter(pos == 3) %>% 
+          inner_join(standings, 
+                     by = "pos", 
+                     suffix = c("1", "2"), 
+                     relationship = "many-to-many"
+                     ) %>% 
+          filter(seed1 > seed2) %>% 
+          mutate(g_max = 3, f_score = 11, pos2 = 4, 
+                 round = self$round, match = 2
+                 ) %>% 
+          select(round, match, pos1 = pos, pos2, seed1, seed2, 
+                 serve1, return1, serve2, return2, g_max, f_score
+                 )
+        
+        round_matches <- bind_rows(round_matches, bronze_match)
+      }
+      
+      round_results <- round_matches %>% 
+        play_many_singles_matches(extra_cols = self$id_cols) %>% 
+        mutate(pos_winner = ifelse(winner == 1, pos1, pos2),
+               pos_loser = ifelse(winner == 1, pos2, pos1)
+        )
+      
+      #If this is not the last round update the standings as normal
+      if(N_matches_round > 1){
+        self$update_standings(round_results)
+      } else {
+        self$update_standings_final(round_results)
+      }
+      
+      #Bind match results
+      self$match_results <- bind_rows(self$match_results, round_results)
+      
+      #Update round and number of matches
+      self$round <- self$round + 1
+      self$N_matches_round <- self$N_matches_round/2
+      
+      return(invisible())
     }
     
   )
-  
-  
-  
   
 )
